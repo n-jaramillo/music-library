7ea0cbc3be90a66637fc3c5ef63bf37dcdd83de8
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 06:49:17 -0800

    moved styles from GalleryItem.js to App.css, updated simpleView and detailView elements

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,46 +3,32 @@ import { useState } from "react"
 function GalleryItem(props) {
     let [view, setView] = useState(false)
 
-    const simpleStyle = {
-        'width': '25vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px'
-    }
-    
-    const detailStyle = {
-        'width': '80vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${props.item.artworkUrl100})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'yellow'
-    }    
-
     const simpleView = () => {
         return (
-            <div style={simpleStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h3>{props.item.collectionName}</h3>
-            </div>
+            <div className='simple'>
+                <div className='simpleHeader'>
+                    <h3>{props.item.trackName}</h3>
+                    <h3>{props.item.collectionName}</h3>
+                </div>
+                <img src={props.item.artworkUrl100} alt={props.item.collectionName} className='simpleBG' />
+            </div >
         )
     }
 
     const detailView = () => {
+        let release = new Date(props.item.releaseDate).getFullYear()
         return (
-            <div style={detailStyle}>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
+            <div className='detailed'>
+                <img src={props.item.artworkUrl100} alt={props.item.collectionName} style={{ 'borderRadius': '3px', 'margin': '0 0 10px' }} />
+                <h2 style={{ 'fontVariant': 'small-caps', 'margin': '0 0 10px' }}>{props.item.trackName}</h2>
+                <h3 style={{ 'margin': '0 0 -5px' }}>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}<br />{release}</h4>
             </div>
         )
     }
 
     return (
-        <div onClick={() => setView(!view)} style={{ display: "inline-block" }}>
+        <div onClick={() => setView(!view)} className='container'>
             {view ? detailView() : simpleView()}
         </div>
     )
