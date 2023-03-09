cdf9c747320b5d1196d0f860f9b2a7f247041375
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 04:35:52 -0800

    part 2 bonus: added reset functionality to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,8 @@
-// import { useState } from "react"
-
 function SearchBar(props) {
     return (
         <form>
-            <input type="text" placeholder="Enter a search term here" onChange={(e) => props.handleSearch(e, e.target.value)} />
-            <input type="submit" />
+            <input type="text" placeholder="Enter a search term here" onChange={(e) => props.handleSearch(e, e.target.value)} onSubmit={(e) => props.handleSearch(e, '')} />
+            <button type="submit">Reset</button>
         </form>
     )
 }
