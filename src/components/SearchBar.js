8d75c869497331d310da46ab34ea312e1a45fc55
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 03:56:15 -0800

    part 2 bonus: refactored SearchBar form handleSearch

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,9 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 function SearchBar(props) {
-    let [searchTerm, setSearchTerm] = useState('')
-
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-            <input type="text" placeholder="Enter a search term here" onChange={(e) => setSearchTerm(e.target.value)} />
+        <form>
+            <input type="text" placeholder="Enter a search term here" onChange={(e) => props.handleSearch(e, e.target.value)} />
             <input type="submit" />
         </form>
     )
