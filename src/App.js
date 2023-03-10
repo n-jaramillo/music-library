15d60757537a84d861d376ea01ee00ea563e1671
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 23:24:30 -0800

    part 3.3: setting the context in App.js for Gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { useEffect, useState } from 'react';
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
 
 function App() {
   let [search, setSearch] = useState('')
@@ -41,7 +42,9 @@ function App() {
         {message}
       </h1>
       <SearchBar handleSearch={handleSearch} />
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div >
   );
 }
