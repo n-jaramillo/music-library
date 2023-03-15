d29ab88214982e4db82bd61e64ff4d89324c5b4a
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-14 22:01:07 -0700

    added useEffect to AlbumView.js to fetch data

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -5,10 +5,29 @@ function AlbumView() {
     const { id } = useParams()
     const [albumData, setAlbumData] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const onlySongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = onlySongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
     return (
-        <div style={{color: "white"}}>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+        <div style={{ color: "white" }}>
+            {renderSongs}
         </div>
     )
 }
