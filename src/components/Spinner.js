7fd4ae1c17773f5d81a39736e2447414b0f431e7
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-15 07:08:28 -0700

    added Loading... header below loading img

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,6 +4,7 @@ const Spinner = () => {
     return (
         <div>
             <img src={loading} alt="loading..." />
+            <h2 className='styledHeader'>Loading...</h2>
         </div>
     )
 }
